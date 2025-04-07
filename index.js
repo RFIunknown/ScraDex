ad70f80813dada5be3a736db4ce02b0584552222
Muhammad Rifai <117048432+RFIunknown@users.noreply.github.com>
2025-04-07 22:19:13 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-(async () => { 
-    try {
-        const { instagramdl, sfiledl, facebookdl, twitterdl, gdrivedl, teraboxdl, threadsdl, snackvideodl } = require("@RFIunknown/ScraDex"); // Import dari module ScraDex
-        const data = await snackvideodl("https://sck.io/p/xP9sDodV");
-        console.log(data); // Output JSON
-    } catch (error) {
-        console.error("Error fetching:", error.message);
-    }
-})();
